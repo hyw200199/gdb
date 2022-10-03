b95e29af93684b000be9cc22ddb0984bdaf65031
huyaowen <2372847321@qq.com>
2022-10-03 15:03:48 -0700

    launch和tasks的配置和make.sh脚本的编写

---
+++ test1/c2-sum.cc
@@ -0,0 +1,5 @@
+#include "c2-sum.h"
+
+int sum(int a,int b){
+    return a+10;
+}
---
+++ test1/c2-test1.cc
@@ -0,0 +1,10 @@
+#include "c2-sum.h"
+#include<bits/stdc++.h>
+
+int main(){
+    int a=100;
+    int b=10;
+    auto c=sum(a,b);
+    printf("c= %d\n",c);
+    return 0;
+}
